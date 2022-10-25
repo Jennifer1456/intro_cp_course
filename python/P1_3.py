e8afa06393794123e5cddb859a302cb13682670e
Jennifer <jey05jey05@gmail.com>
2022-10-25 14:08:23 +0800

    10/25

---
+++ python/P1_3.py
@@ -0,0 +1,8 @@
+A_over_B = complex(0., 0.)
+### START YOUR CODE HERE ###
+A = complex(1, 3)
+B = complex(5, 7)
+A_over_B = A/B
+
+#### END YOUR CODE HERE ####
+print('value:', A_over_B)
---
+++ python/P1_4.py
@@ -0,0 +1,8 @@
+x = 12.3
+y = 4.56
+func = 0.
+### START YOUR CODE HERE ###
+
+
+#### END YOUR CODE HERE ####
+print('value:', func)
